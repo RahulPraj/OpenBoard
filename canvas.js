8b74a877e9a7c33114be9a975483d9c25b58976b
RahulPraj <rahul971801@gmail.com>
2021-12-10 23:09:25 +0530

    undo-redo done

--- canvas.js
+++ canvas.js
@@ -4,11 +4,17 @@ canvas.height =window.innerHeight;
 let pencilColorCont = document.querySelectorAll(".pencil-color");
 let pencilWidthElem = document.querySelector(".pencil-width");
 let eraserWidthElem = document.querySelector(".eraser-width");
-
+let download = document.querySelector(".download");
+let redo = document.querySelector(".redo");
+let undo = document.querySelector(".undo");
 let penColor = "red";
 let eraserColor = "white";
 let penWidth = pencilWidthElem.value;
 let eraserWidth = eraserWidthElem.value;
+
+let undoRedoTracker = [];//data
+let track = 0; //represnt which action from tacker array
+
 let mouseDown = false;
 //drawing graphic eka path mai rahega -> API
 let tool = canvas.getContext("2d");//api tool to perform the graphic
@@ -36,13 +42,51 @@ canvas.addEventListener("mousedown", (e) => {
 canvas.addEventListener("mousemove", (e) => {
     if(mouseDown) drawStroke({
       x: e.clientX,
-      y: e.clientY    
+      y: e.clientY ,
+      color: eraserFlag ? eraserColor : penColor,
+      width: eraserFlag ? eraserWidth : penWidth  
     })
     
 })
 canvas.addEventListener("mouseup", (e)  => {
     mouseDown = false;
+
+    let url = canvas.toDataURL();
+    undoRedoTracker.push(url);
+    track = undoRedoTracker.length-1;
 })
+undo.addEventListener("click", (e) => {
+    if (track > 0) track--;
+    // track action
+    let trackObj ={
+        trackValue: track,
+        undoRedoTracker
+    }
+    undoRedoCanvas(trackObj);
+})
+
+redo.addEventListener("click", (e) => {
+    if(track < undoRedoTracker.length-1) track++;
+    // track action
+    let trackObj ={
+        trackValue: track,
+        undoRedoTracker
+    }
+    undoRedoCanvas(trackObj);
+})
+
+function undoRedoCanvas(trackObj) {
+    track = trackObj.trackValue;
+    undoRedoTracker = trackObj.undoRedoTracker;
+
+    let url = undoRedoTracker[track];
+    let img  = new Image(); //new image refrence element
+    img.src = url;
+    img.onload = (e) => {
+        tool.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+
+}
 
 function beginPath(strokeObj) {
     tool.beginPath();
@@ -50,6 +94,8 @@ function beginPath(strokeObj) {
 }
 
 function drawStroke(strokeObj) {
+    tool.strokeStyle = strokeObj.color;
+    tool.lineWidth = strokeObj.width;
     tool.lineTo(strokeObj.x, strokeObj.y);
     tool.stroke();
 }
@@ -70,7 +116,7 @@ pencilWidthElem.addEventListener("change", (e) => {
 
 eraserWidthElem.addEventListener("change", (e) => {
     eraserWidth = eraserWidthElem.value;
-    tool.lineWidth = penWidth;
+    tool.lineWidth = eraserWidth;
     
 })
 eraser.addEventListener("click", (e) => {
@@ -82,4 +128,13 @@ eraser.addEventListener("click", (e) => {
         tool.strokeStyle = penColor;
         tool.lineWidth = penWidth;
     }
+})
+// download the  canvas page
+download.addEventListener("click", (e) => {
+    let url = canvas.toDataURL();
+
+    let a  = document.createElement("a");
+    a.href = url;
+    a.download = "board.jpg";
+    a.click();
 })
